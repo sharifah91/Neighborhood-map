7e8867a5f6f79cd3711b5ac91ca7db3d8853134d
sharifah91 <alamersharifah@gmail.com>
2017-10-13 17:45:35 +0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -550,7 +550,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < (window.innerHeight / s) *cols; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images\pizza.png";
+    elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
